8e070fdaeedb1450996b8384517d6aab9c860808
R-Vulpes <1982747519@qq.com>
2023-10-27 17:26:20 +0800

    Add files via upload

---
+++ code/class20/test.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// static修饰局部变量
+// void test()
+// {
+//     static int a = 1;
+//     a++;
+//     printf("%d\n", a);
+// }
+// int main()
+// {
+//     int i = 0;
+//     while(i<10)
+//     {
+//         test();
+//         i++;
+//     }
+//     // printf("%d\n", a);
+//     system("pause");
+//     return 0;
+// }
+
+int main()
+{
+    extern int g_val;//不知道为什么err
+    // int g_val = 1;
+    printf("%d\n", g_val);
+    system("pause");
+    return 0;
+}
+// int main()
+// {
+//     system("pause");
+//     return 0;
+// }
