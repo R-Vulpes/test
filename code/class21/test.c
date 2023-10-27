175febadb806a591f8e4c0360e9fb28b29c286aa
R-Vulpes <1982747519@qq.com>
2023-10-27 17:59:29 +0800

    Create test.c

---
+++ code/class21/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//define 定义标识符常量
+#define num 100
+
+//define 定义宏
+//宏有参数
+
+int main()
+{
+    printf("%d\n",num);
+    system("pause");
+    return 0;
+}
+
+// int main()
+// {
+//     system("pause");
+//     return 0;
+// }
