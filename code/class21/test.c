615ec8bc4e0d8496b1072ead98431229163c588b
R-Vulpes <1982747519@qq.com>
2023-10-30 10:17:35 +0800

    Update test.c

--- code/class21/test.c
+++ code/class21/test.c
@@ -6,11 +6,22 @@
 #define num 100
 
 //define 定义宏
-//宏有参数
+//宏有参数,参数无类型 
+
+// #define ADD(x,y) x+y //((x)+(y))
+// int main()
+// {
+//     int a = 10;
+//     int b = 20;
+//     int c = ADD(a,b);
+//     printf("%d\n",c);
+//     system("pause");
+//     return 0;
+// }
 
 int main()
 {
-    printf("%d\n",num);
+    
     system("pause");
     return 0;
 }
