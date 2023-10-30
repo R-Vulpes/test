1060e15c2f8bbe8048ff4c9511e0a502b7ef553f
R-Vulpes <1982747519@qq.com>
2023-10-30 11:46:33 +0800

    Update test_point.c

--- code/class23/test_point.c
+++ code/class23/test_point.c
@@ -5,23 +5,35 @@
 // 内存单元的编号=地址=指针
 // 存放指针（地址）的变量就是指针变量
 
+// int main()
+// {
+//     int a = 10;
+//     &a;
+//     printf("%p\n", &a); 
+//     //%p打印地址
+
+//     int* p = &a;
+//     //*说明p为指针变量；int说明p指向的对象是指针类型
+
+//     printf("%p\n", p);
+
+//     *p = 20;
+//     //解引用操作符，找到p指向的对象
+    
+//     printf("%d\n", a); 
+    
+//     system("pause");
+//     return 0;
+// }
+
 int main()
 {
-    int a = 10;
-    // &a;
-    printf("%p\n", &a); 
-    //%p打印地址
-
-    int* p = &a;
-    //*说明p为指针变量；int说明p指向的对象是指针类型
-
-    printf("%p\n", p);
-
-    *p = 20;
-    //解引用操作符，找到p指向的对象
-    
-    // printf("%d\n", a); 
-    
     system("pause");
     return 0;
 }
+
+// int main()
+// {
+//     system("pause");
+//     return 0;
+// }
