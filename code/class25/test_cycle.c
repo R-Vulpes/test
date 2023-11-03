3459afc3f9382cec94559da83ae95254877c79e4
三七分 <1982747519@qq.com>
2023-11-03 17:42:28 +0800

    Update test_cycle.c

--- code/class25/test_cycle.c
+++ code/class25/test_cycle.c
@@ -71,39 +71,58 @@
 // }
 
 // switch语句
-int main()
-{
-    int day = 0;
-    scanf("%d", &day);
-    switch (day)
-    // switch(必须为 整型表达式),case决定入口，break决定出口
-    // case后面必须为 整型常量表达式（字符也行，ascii码值为整）
-    {
-    case 1:
-        printf("星期一\n");
-        break;
-    case 2:
-        printf("星期二\n");
-        break;
-    case 3:
-        printf("星期三\n");
-        break;
-    case 4:
-        printf("星期四\n");
-        break;
-    case 5:
-        printf("星期五\n");
-        break;
-    case 6:
-        printf("星期六\n");
-        break;
-    case 7:
-        printf("星期日\n");
-        break;
-    }
-    system("pause");
-    return 0;
-}
+// int main()
+// {
+//     int day = 0;
+//     scanf("%d", &day);
+//     switch (day)
+//     // switch(必须为 整型表达式),case决定入口，break决定出口
+//     // case后面必须为 整型常量表达式（字符也行，ascii码值为整）
+//     // {
+//     // case 1:
+//     //     printf("星期一\n");
+//     //     break;
+//     // case 2:
+//     //     printf("星期二\n");
+//     //     break;
+//     // case 3:
+//     //     printf("星期三\n");
+//     //     break;
+//     // case 4:
+//     //     printf("星期四\n");
+//     //     break;
+//     // case 5:
+//     //     printf("星期五\n");
+//     //     break;
+//     // case 6:
+//     //     printf("星期六\n");
+//     //     break;
+//     // case 7:
+//     //     printf("星期日\n");
+//     //     break;
+//     // }
+
+//     {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         printf("weekday\n");
+//         break;
+//     case 6:
+//     case 7:
+//         printf("weekend\n");
+//         break;
+//     default:
+//     // 分支到其他值，跳转到default
+//         printf("选择错误\n");
+//         break;
+//     }
+
+//     system("pause");
+//     return 0;
+// }
 
 // int main()
 // {
