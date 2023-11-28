eeed88525e1e9d2391b9eb519a914f607c99d0cd
三七分 <1982747519@qq.com>
2023-11-28 18:11:08 +0800

    1127

---
+++ code/class33/test_dowhile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 0;
+    do
+    {
+        i++;
+        printf("%d ", i);
+    } while (i < 10);
+
+    system("pause");
+    return 0;
+}
+// int main()
+// {
+//     system("pause");
+//     return 0;
+// }
--- code/class33/test_for.c
+++ code/class33/test_for.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int i = 0;
-    for (i = 1; i <= 10; i++)
-    {
-        printf("%d\n", i);
-    }
-    system("pause");
-    return 0;
-}
+// int main()
+// {
+//     int i = 0;
+//     for (i = 1; i <= 10; i++)
+//     {
+//         printf("%d\n", i);
+//     }                           
+//     system("pause");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int arr[]={1,2,3,4,5,6,7,8,9,10};
+//     printf("%d\n",arr[10]);
+//     system("pause");
+//     return 0;
+// }
+
+// int main()
+// {
+//     system("pause");
+//     return 0;
+// }
