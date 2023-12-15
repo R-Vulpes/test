c2cb345b3e97551695585e498345a5f8757c0fd0
三七分 <1982747519@qq.com>
2023-12-15 11:39:14 +0800

    Update test_for.c

--- code/class33/test_for.c
+++ code/class33/test_for.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // int main()
 // {
@@ -19,6 +20,35 @@
 //     system("pause");
 //     return 0;
 // }
+
+// 模拟用户登录场景，只能登录三次（密码正确则登录成功，三次都输入失败则退出程序）
+//比较两个字符串是否相等，不能用==，要用库函数strcmp;如果返回值为0，表示两个字符串相等
+int main()
+{
+    int i = 0;
+    char pass[10] = "12345678";
+    char arr[10] = { 0 };
+    printf("please input your password:");
+    for(i ;i<3; i++)
+    {
+        scanf("%s", &arr);
+        if (strcmp(arr,pass)==0) 
+        {
+            printf("welcome to bilibili!\n");
+            break;
+        }
+        else
+        {
+            printf("password is fault,input again:");
+        }
+    }
+    if(i==3)
+    {
+        printf("you have no chance.");
+    }
+    system("pause");
+    return 0;
+}
 
 // int main()
 // {
